7a058f3412a8fcf96a821a14d48dbc74eeb9400b
Nguyen Duc An <nguyenducan11042001@gmail.com>
2024-05-16 23:52:10 +0700

    chore: :wrench: update eslint configuration
    - Fix lint script typo.
    - Update eslint config file.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,5 +6,10 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic
+  ...tseslint.configs.stylistic,
+  {
+    rules: {
+      '@typescript-eslint/unified-signatures': 'warn'
+    }
+  }
 );
